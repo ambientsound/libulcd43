2be1562111a75b394ae46f93b7cb88160ea37162
Kim Tore Jensen <kim@incendio.no>
2013-11-15 22:35:21 +0100

    Rename check_touch to check_ulcd.

---
+++ tests/check_ulcd.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <check.h>
+#include "../src/ulcd43.h"
+
+struct ulcd_t *ulcd;
+
+void
+setup(void)
+{
+    ulcd = ulcd_new();
+    ulcd->port = "/dev/ttyAMA0";
+    ulcd->baudrate = 115200;
+
+    if (ulcd_open_serial_port(ulcd)) {
+        ck_abort_msg("Could not open serial port.");
+    }
+    ulcd_set_serial_port_parameters(ulcd);
+}
+
+void
+teardown(void)
+{
+    ulcd_free(ulcd);
+}
+
+/**
+ * Touch test case
+ */
+
+START_TEST (test_touch_get)
+{
+    param_t status;
+    ulcd_touch_get(ulcd, TOUCH_GET_MODE_STATUS, &status);
+    ck_assert(status == TOUCH_STATUS_NOTOUCH);
+}
+END_TEST
+
+START_TEST (test_touch_get_event)
+{
+    struct touch_event_t ev;
+    ev.point.x = 0;
+    ev.point.y = 0;
+    ulcd_touch_get_event(ulcd, &ev);
+    ck_assert(ev.status == TOUCH_STATUS_NOTOUCH);
+    ck_assert(ev.point.x == 0);
+    ck_assert(ev.point.y == 0);
+}
+END_TEST
+
+Suite *
+ulcd_suite(void)
+{
+    Suite *s = suite_create("uLCD43");
+
+    /* Touch test case */
+    TCase *tc_touch = tcase_create("Touch");
+    tcase_add_unchecked_fixture(tc_touch, setup, teardown);
+    tcase_add_test(tc_touch, test_touch_get);
+    tcase_add_test(tc_touch, test_touch_get_event);
+    suite_add_tcase(s, tc_touch);
+
+    return s;
+}
+
+int
+main(void)
+{
+    int number_failed;
+    Suite *s = ulcd_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
